6f3bbac0c4e44ccbd76097aea4ba7a94d6c2b57c
Darek P <dajmos007@gmail.com>
2017-12-10 13:04:00 +0100

    Nowy plik

---
+++ number.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$x = 40593;
+
+echo $x%2==0 ?'Jest Parzysta': "Jest Nieparzysta ";
+
+
+$x = 10;
+echo $>0 &&$x<10 ? "Cyfra": "liczba";
+
+echo " jest";
+echo $x%2? "nie": "";
+echo "Parzysta";
--- ternerius.php
+++ ternerius.php
@@ -6,6 +6,7 @@
  * Time: 12:40
  */
 
-$x = 24;
+$x = 40593;
 
 echo $x%2==0 ?'Jest Parzysta': "Jest Nieparzysta ";
+
