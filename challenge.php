b6da768ded6055dd364b284c0c05badc32fc8389
Darek P <dajmos007@gmail.com>
2017-12-10 12:10:54 +0100

    Dodanie nowego pliku

---
+++ challenge.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 12:05
+ */
+
+
+$a=5;
+$b= a +4/2 *3**2;
+echo $b, PHP_EOL;
+
+// 5 + 4/2 * 3 do potegi 2
+//5 + 4/2 *9
+//5+18 = 23
+
