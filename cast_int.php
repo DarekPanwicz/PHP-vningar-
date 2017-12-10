330582b61e3be6206e51f31792fedcfbc2ab602a
Darek P <dajmos007@gmail.com>
2017-12-10 15:04:36 +0100

    Poprawieie syntaxa int

--- cast_int.php
+++ cast_int.php
@@ -3,30 +3,35 @@
  * Created by PhpStorm.
  * User: Darek
  * Date: 2017-12-10
- * Time: 14:48
+ * Time: 14:58
  */
 
-
-$var= 5; 5.5; null; false; true; 'php'; '4php';
+$var= 5;
 $cast= (int) $var;
 var_dump($var, $cast);
 
-
-$var= 5; 5.5; null; false; true; 'php'; '4php';
+var=  5.5;
 $cast= (int) $var;
 var_dump($var, $cast);
 
-
-$var= 5; 5.5; null; false; true; 'php'; '4php';
+var=  null;
 $cast= (int) $var;
 var_dump($var, $cast);
 
-
-$var= 5; 5.5; null; false; true; 'php'; '4php';
+var= false;
 $cast= (int) $var;
 var_dump($var, $cast);
 
-
-$var= 5; 5.5; null; false; true; 'php'; '4php';
+var=  true;
 $cast= (int) $var;
 var_dump($var, $cast);
+
+var= 'php';
+$cast= (int) $var;
+var_dump($var, $cast);
+
+var= '4php';
+$cast= (string) $var;
+var_dump($var, $cast);
+
+
