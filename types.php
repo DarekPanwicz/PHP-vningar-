e8b147b1397e22e592ab0651667722fc22ed062c
Darek P <dajmos007@gmail.com>
2017-12-10 14:29:10 +0100

    Nowy plik

---
+++ types.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 14:27
+ */
+
+$typesExamples = [
+    0, -6, 14
+    true, false,
+    null,
+    3.1415, "Darek",
+    "", PHP_EOL,
+
+
+];
+var_dump($typesExamples)
