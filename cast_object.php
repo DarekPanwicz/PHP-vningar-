89f5af204869ab48bdd53407d24cbd9ce786fac4
Darek P <dajmos007@gmail.com>
2017-12-10 15:08:13 +0100

    Nowy plik

---
+++ cast_object.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 14:58
+ */
+
+$var= 5;
+$cast= (object) $var;
+var_dump($var, $cast);
+
+var=  5.5;
+$cast= (object) $var;
+var_dump($var, $cast);
+
+var=  null;
+$cast= (object) $var;
+var_dump($var, $cast);
+
+var= false;
+$cast= (object) $var;
+var_dump($var, $cast);
+
+var=  true;
+$cast= (object) $var;
+var_dump($var, $cast);
+
+var= 'php';
+$cast= (object) $var;
+var_dump($var, $cast);
+
+var= '4php';
+$cast= (object) $var;
+var_dump($var, $cast);
