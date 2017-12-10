eb105275b6659712a99fc64cea705d7714aba054
Darek P <dajmos007@gmail.com>
2017-12-10 14:38:01 +0100

    Dodanie nowego pliku

---
+++ second.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 14:35
+ */
+
+$firstName = "Darek";
+$secondName = $firstName;
+echo "My first name is {$firstName}.", PHP_EOL;
+echo "My second name is {$secondName}.",PHP_EOL;
