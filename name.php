5e2ba68897aef5cce9c3cde369ccf2bc18f125e2
Darek P <dajmos007@gmail.com>
2017-12-10 13:59:55 +0100

    Dodanie nowego pliku

---
+++ name.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 13:55
+ */
+
+$name= 'Radek';
+$text= "My name is {$name}.";
+echo $text, PHP_EOL;
+var_dump ($name, $text);
