2aea06d23b23b4d4b8eb91ea0176f16046cbc05b
Darek P <dajmos007@gmail.com>
2017-12-10 13:38:30 +0100

    Dodany nowy plik

---
+++ przepisanie.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 13:27
+ */
+
+$a= "";
+$a .= "swieta";
+$a .= "juz";
+$a .= "niedlugo";
---
+++ triangle.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 13:29
+ */
+
+$a = 5;
+echo "Wynik liczenia"." ".(($a**2)*(3**0.5))/4;
