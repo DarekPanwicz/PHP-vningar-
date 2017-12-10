729f0e002c8748727d34c3604402f588ee9fb255
Darek P <dajmos007@gmail.com>
2017-12-10 14:41:44 +0100

    Zmiana w imieniu

--- second.php
+++ second.php
@@ -8,6 +8,6 @@
 
 $firstName = "Darek";
 $secondName = $firstName;
-$decondName = $Batman;
+$decondName = 'Batman';
 echo "My first name is {$firstName}.", PHP_EOL;
 echo "My second name is {$secondName}.",PHP_EOL;
