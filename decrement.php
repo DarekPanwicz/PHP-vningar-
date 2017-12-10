559406af4e2d681371f2ed60c1bf45ae51503914
Darek P <dajmos007@gmail.com>
2017-12-10 12:39:29 +0100

    Dodany nowy plik

---
+++ decrement.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 12:26
+ */
+
+
+$x=4;
+
+echo "pomniejszylismy wartosc zmiennej $x=".$x." o jeden i otrzymalismy .--$x.", PHP_EOL;
---
+++ simple.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 12:18
+ */
+
+echo 2+1, PHP_EOL;
+echo 5-2 , PHP_EOL;
+echo 1*3, PHP_EOL;
+echo 9/3, PHP_EOL;
+echo 9**0.5, PHP_EOL;
+echo 12%15, PHP_EOL;
