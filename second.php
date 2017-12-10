80bad3ab612be5aeae9901666995b9aa585043a4
Darek P <dajmos007@gmail.com>
2017-12-10 14:43:59 +0100

    Naprawienie bledu

--- second.php
+++ second.php
@@ -7,7 +7,7 @@
  */
 
 $firstName = "Darek";
-$secondName = $firstName;
-$decondName = 'Batman';
+$secondName = &$firstName;
+$secondName = 'Batman';
 echo "My first name is {$firstName}.", PHP_EOL;
 echo "My second name is {$secondName}.",PHP_EOL;
