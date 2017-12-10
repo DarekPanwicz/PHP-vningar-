efe73e6ee9fd955869907097103c293099a480ec
Darek P <dajmos007@gmail.com>
2017-12-10 15:13:30 +0100

    Nowy plik

---
+++ file.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-10
+ * Time: 15:10
+ */
+
+$a= 3;
+include 'another.php';
+#require 'another.php';
+
+echo $a, PHP_EOL;
+
+/**
+ * include warning
+ */
