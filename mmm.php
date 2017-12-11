7d8e9d55503af309fa41eda271cbe1b0e2966cd9
dajmos <dajmos007@gmail.com>
2017-12-11 14:49:33 +0100

    New File

---
+++ mmm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-11
+ * Time: 14:38
+ */
+
+$counter =2;
+// 0 - false
+if ($counter ==true){
+
+    echo '<strong> "Ta liczba to true" </strong>';
+} else {
+
+    echo "To jest false";
+}
