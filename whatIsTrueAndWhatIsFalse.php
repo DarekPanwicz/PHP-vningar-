f62c02c2feb6f85694e05f1e1c695bdbe0943bc2
dajmos <dajmos007@gmail.com>
2017-12-11 15:37:53 +0100

    Adding new counter

--- whatIsTrueAndWhatIsFalse.php
+++ whatIsTrueAndWhatIsFalse.php
@@ -8,10 +8,27 @@
 
 $counter =2;
 // 0 - false
+//String to 0 czyli false
+
 if ($counter ==true){
 
     echo '<strong> "Ta liczba to true" </strong>';
 } else {
 
     echo "To jest false";
+}
+
+
+echo "<br><br><br><br><br><br>";
+
+
+
+$counter =0;
+// 0 - false
+if ($counter ==true){
+
+    var_export("Pokazuje zawartosc zmiennej: $counter ") ;
+} else {
+
+    var_dump( $counter."Pokazuje zawartosc zmiennej:");
 }
