57717030f9ebd75fea3ff6447399cbaca8f12684
dajmos <dajmos007@gmail.com>
2017-12-12 14:49:58 +0100

    Repetitive elements, in two tables

--- tabs2.php
+++ tabs2.php
@@ -8,16 +8,39 @@
 
 $tabelka1= array(
 
-    "zwierze"=>"kot",
-    "Zwierze"=>"Pies",
-    "Zwierze"=>"malpa",
+    "zwierze1"=>"kot",
+    "zwierze2"=>"Pies",
+    "zwierze3"=>"malpa",
+    "ryba3"=>"Szczupak",
 );
 
 $tabelka2 = array(
 
-    "ryba"=>"Losos",
-    "ryba"=>"Szczupak",
-    "ryba"=> "Pstrag"
+    "ryba1"=>"Losos",
+    "ryba2"=>"Szczupak",
+    "ryba3"=> "Pstrag"
 
 );
 
+
+echo "<pre>";
+echo "<hr>";
+
+print_r($tabelka1);
+echo "<hr>";
+
+
+echo "<pre>";
+echo "<hr>";
+
+print_r($tabelka2);
+echo "<hr>";
+
+$arrTotal = array_intersect($tabelka1, $tabelka2);
+
+
+echo "<pre>";
+echo "<hr>";
+
+print_r($arrTotal);
+echo "<hr>";
