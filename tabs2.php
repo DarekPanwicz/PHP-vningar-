f16a91f5cf22b437df26d8f91a74d379965f2d5b
dajmos <dajmos007@gmail.com>
2017-12-12 14:36:39 +0100

    New file

---
+++ tabs2.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-12
+ * Time: 14:29
+ */
+
+$tabelka1= array(
+
+    "zwierze"=>"kot",
+    "Zwierze"=>"Pies",
+    "Zwierze"=>"malpa",
+);
+
+$tabelka2 = array(
+
+    "ryba"=>"Losos",
+    "ryba"=>"Szczupak",
+    "ryba"=> "Pstrag"
+
+);
+
