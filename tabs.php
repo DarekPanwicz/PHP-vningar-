499bf9de85a53a63c236bcf393afab5deff60688
dajmos <dajmos007@gmail.com>
2017-12-12 14:12:23 +0100

    Working with tables

--- tabs.php
+++ tabs.php
@@ -6,7 +6,7 @@
  * Time: 08:34
  */
 
-$TableOne = Array ("hund", "katt", "Häst", "Hippopotam", "Björn");
+$tableOne = Array ("hund", "katt", "Häst", "Hippopotam", "Björn");
 
 
 
@@ -19,15 +19,45 @@ $tableTwo= Array(
 
 );
 
+$addText =" Tekst dodany do tabelki XX";
 
 
 
 
 
 
+print_r ("<pre>Przed dodaniem nowych elementow ".$tableOne."</pre>") ;
 
-    print_r($TableOne) ;
+    "<br> <br> <br> ";
 
-    "<br> <br><br> ";
+print_r ("<pre>Po dodaniu nowych elementow </pre>") ;
+$ar1 = array_push($tableOne, $addText);
 
-    echo $tableTwo;
+
+
+echo "<pre>";
+var_export($arr1);
+echo "</pre>";
+
+// unset - usuniecie tablicy, zmiennej itd
+
+
+//zamiana stringa na tabelke
+
+$string =" hmm no nie wiem co dzis sobie zjem na sniadanie";
+
+$zamiana = explode(" ",$string);
+echo "<pre>"."print_r";
+print_r($zamiana);
+echo "</pre>";
+"<hr><br>";
+
+echo "<pre>"."var dump";
+var_dump($zamiana);
+echo "</pre>";
+"<hr><br>";
+
+echo "<pre>";
+echo "echo".$zamiana;
+echo "</pre>";
+"<hr><br>";
