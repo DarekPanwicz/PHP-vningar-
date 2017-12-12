ffd8937571bde16311b3239d6b289e44efb3aac3
dajmos <dajmos007@gmail.com>
2017-12-12 18:47:44 +0100

    New code for game development

--- game.php
+++ game.php
@@ -6,3 +6,26 @@
  * Time: 18:32
  */
 
+$tabelka1 = array(
+
+    "Zwierzatko" => "Piesola",
+    "Zwierzatko2"=> "tygrysa",
+    "Zwierzatko3"=> "Malpe",
+    "wygrana" =>"Samochod",
+    "Wygrana1" => "Dom",
+    "Wygrana2" => "Rower",
+
+);
+
+
+
+
+$losowanie = count($tabelka1);
+$maxIlosc= $losowanie -1;
+
+//Nie dokonczony kod trzeba jeszcze zmienic tablice i dodac ceny do kazdego losowania
+echo "<pre">;
+
+print_r( "Uwaga <br> W dzisiejszym lodowaniu padla glowna nagroda".$tabelka1[1]."O wartosci".)
+
+echo "</pre">;
