34661d072b73d99c9a721ee39396b37acd0621db
dajmos <dajmos007@gmail.com>
2017-12-12 08:51:09 +0100

    Add new file

---
+++ tabs.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-12
+ * Time: 08:34
+ */
+
+$TableOne = Array ("hund", "katt", "Häst", "Hippopotam", "Björn");
+
+
+
+$tableTwo= Array(
+
+    "Djur"=>"Elg",
+    "fisk"=> "Lax",
+    "Djur"=> "Elephant",
+    "Fågel"=> "En ö",
+
+);
+
+
+
+
+
+
+
+
+    print_r($TableOne) ;
+
+    "<br> <br><br> ";
+
+    echo $tableTwo;
