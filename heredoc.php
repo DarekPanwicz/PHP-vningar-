e19cb6016a20293e54ee6829ee4c446e3bc225a2
dajmos <dajmos007@gmail.com>
2017-12-13 12:11:23 +0100

    nl2br

--- heredoc.php
+++ heredoc.php
@@ -5,3 +5,15 @@
  * Date: 2017-12-13
  * Time: 11:37
  */
+$zmienna ="MIAUUU...";
+$pies = <<<PL
+
+ Wow to rzeczywiscie <strong>dziala: </strong> $zmienna
+ no sam nie wiem co wiecej
+ moglbym powiedziec ale
+ to naprawde dziala
+
+PL;
+
+
+echo nl2br($pies);
