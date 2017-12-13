ca080af485aa659bb3943f22fc0c9bdde8b31068
dajmos <dajmos007@gmail.com>
2017-12-13 16:37:07 +0100

    First custom function

--- function.php
+++ function.php
@@ -5,3 +5,44 @@
  * Date: 2017-12-13
  * Time: 16:03
  */
+
+$pies="Reks";
+$glodny =true;
+
+
+
+function jedzenie($glodny){
+
+    if($glodny === true){
+
+        return true;
+
+
+    } else if($glodny === false){
+
+        return false;
+
+    };
+
+
+};
+
+
+$wynik = jedzenie($glodny);
+
+ if ($wynik ===true){
+
+     echo "<bre>";
+     echo "Daj jedzonko dla psa";
+     echo "</bre>";
+
+ } else if ($glodny===false){
+
+     echo "<bre>";
+     echo "Piesol juz jest najedzony";
+     echo "</bre>";
+
+ }
+
+
+
