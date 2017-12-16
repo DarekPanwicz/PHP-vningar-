7ffeab870de11638220e862563e7bb77a7a5e159
Darek P <dajmos007@gmail.com>
2017-12-16 13:36:34 +0100

    New switch

---
+++ china.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-16
+ * Time: 13:12
+ */
+
+$year =2017;
+$rest = $year %12;
+switch ($year %12){
+
+    case 0:
+        echo "Malpa";
+        break;
+
+    case 1:
+        echo "Szczor";
+        break;
+
+    case 2:
+        echo "Kot";
+        break;
+
+    case 3:
+        echo "Swinia";
+        break;
+
+
+
+
+        //dokonczyc w domu
+
+
+
+
+
+
+
+}
