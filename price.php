69ed50b6b5bffd56a68b96fc17e4578c316dfa83
Darek P <dajmos007@gmail.com>
2017-12-16 16:01:18 +0100

    New file

---
+++ price.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-16
+ * Time: 14:57
+ */
+
+$liczba = 34;
+
+$arr1 = [
+
+    "jeden", "dwa", "trzy", "cztery", "piec", "szesc", "siedem", "osiem", "dziewiec",
+
+];
+
+
+
+$arr2 = [
+
+    "zlotych", "zloty", "zlote", "zlote", "zlote", "zlotych", "zlotych", "zlotych", "zlotych", "zlotych"
+
+];
+
+
+
+echo $total =  $arr1[$liczba]."".$arr2[$liczba], PHP_EOL;
+
+    if($liczba > 20){
+
+        $arr3 = [
+
+            'dwadziscia', 'trzydzisci', 'czterdzisci', 'piedzisiat', 'szescdzisiat', 'siedemdzisiat', 'osiemdzisiat', 'dziewidzisiat'
+
+        ];
+
+        $ileDziesiatki = $ilestring {0};
+        $ileJednosci = $ilestring{1};
+        echo $arr3[$ileDziesiatki -2], "";
+
+
+
+    };
+
+
+//Zadanie domowe
---
+++ random.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-16
+ * Time: 15:42
+ */
+
+
+for($i=0; $i<10; $i++){
+
+
+   do {
+       $r = rand(1, 1000);
+
+   }   while ($r %2);
+
+       echo $r;
+
+
+
+    };
+
+};
+
+
+
+
+
+
