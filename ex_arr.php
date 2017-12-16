b210aefc8311f422c0b3e9d32565eb307c1aa8f2
Darek P <dajmos007@gmail.com>
2017-12-16 10:05:57 +0100

    Adding new line of code

--- ex_arr.php
+++ ex_arr.php
@@ -5,9 +5,26 @@
  * Date: 2017-12-16
  * Time: 09:49
  */
+
+echo "<pre> <h1>";
 $exArr1 = [
 'klucz'=> 'wartosc',
 
 ];
 
 var_export($exArr1);
+
+$exArr2 = [
+
+    "wartosc",
+];
+
+var_export($exArr2);
+
+
+
+$exArr3['klucz']= 'wartosc';
+
+var_export($exArr3)
+
+echo "</pre></h1>";
