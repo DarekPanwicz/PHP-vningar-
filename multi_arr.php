fe78e4c5f2b39260b629ec7d153ebf55be2ffede
Darek P <dajmos007@gmail.com>
2017-12-16 10:43:40 +0100

    New file

---
+++ multi_arr.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-16
+ * Time: 10:41
+ */
+
+$big = [
+    "small"=> [
+
+        'key'=> "Value",
+    ],
+
+
+];
+
+var_export($big);
