94f8b2c8440969dbd86519467e015e0633aee62e
Darek P <dajmos007@gmail.com>
2017-12-17 15:21:48 +0100

    New object

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
  * Date: 2017-12-17
  * Time: 15:15
  */
+declare(stric_type=1);
 
-require_once  'customer.php';
+require_once 'Customer.php';
 
 $customer =new Customer();
+$customer->name = 'Marian';
+
 var_export($customer);
