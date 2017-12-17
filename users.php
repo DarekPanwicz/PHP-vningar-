9ee71a65a61378b2dec66ef1e93b8825997340e5
Darek P <dajmos007@gmail.com>
2017-12-17 14:51:13 +0100

    New file

---
+++ users.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-17
+ * Time: 13:31
+ */
+
+declare(strict_types=1);
+
+$users = [
+    [
+        'name' => 'Arkadiusz',
+        'birthYear' => 1977,
+        'hobby' => 'stamps',
+        'sport' => 'basketball',
+        'occupation' => 'programmer',
+        'status' => 'hidden',
+    ],
+    [
+        'name' => 'Nadia',
+        'birthYear' => 1988,
+        'hobby' => 'sport',
+        'sport' => 'volleyball',
+        'occupation' => 'singer',
+        'status' => 'activated',
+    ],
+    [
+        'name' => 'Klara',
+        'birthYear' => 1988,
+        'hobby' => 'sport',
+        'sport' => 'volleyball',
+        'occupation' => 'programmer',
+        'status' => 'disabled',
+    ],
+    [
+        'name' => 'Juliusz',
+        'birthYear' => 1984,
+        'hobby' => 'sport',
+        'sport' => 'sqush',
+        'occupation' => 'programmer',
+        'status' => 'activated',
+    ],
+];
+
+/*foreach ($users as $usersValue['status'])
+
+$newUser= [];
+
+{
+
+    if ($usersValue['status'] == "hidden")
+        {
+            var_export("Tu jest hidden ".$usersValue);
+            continue;
+    }
+
+    elseif ($usersValue['status'] == "disabled")
+    {
+        var_export("Tu jest disabled ".$usersValue);
+        continue;
+    }
+
+    elseif ($usersValue['status'] < 1987)
+    {
+        var_export("Urodzony wczesniej ".$usersValue);
+        continue;
+    }
+
+}
+
+$newUsers =[$usersValue];
+var_dump($newUsers);*/
+
+
+
