007e34c21c031cb536289df8c6ea25e4985a4133
Darek P <dajmos007@gmail.com>
2017-12-17 11:56:11 +0100

    New file

---
+++ own_function.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek
+ * Date: 2017-12-17
+ * Time: 11:53
+ */
+
+function printFamilyName($familyName){
+
+    echo "Name: $familyName", PHP_EOL;
+}
+
+printFamilyName("Nowak");
+printFamilyName("Kowalski")
