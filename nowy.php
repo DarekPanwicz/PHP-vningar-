dc7e35c82e7970066d80cb2edf7f6af0df6d7e75
Darek P <dajmos007@gmail.com>
2017-12-17 10:46:05 +0100

    New function

--- nowy.php
+++ nowy.php
@@ -5,3 +5,19 @@
  * Date: 2017-12-10
  * Time: 09:28
  */
+
+
+// mb zmiana kodowania przy funkcjach
+
+
+$strings =['Navicat', 'Category','Catharsis'];
+
+foreach ($strings as $nazwa){
+
+    echo mb_strpos($nazwa, "cat");
+    echo mb_strlen ( $nazwa);
+    echo mb_str_replace("cat", "dog", "zmieniona wartosc");
+
+
+};
+
