d491a514607c33571bc576ceda26de39ae5499d5
Darek P <dajmos007@gmail.com>
2017-12-17 12:43:08 +0100

    New functions added

--- return_fun.php
+++ return_fun.php
@@ -15,16 +15,9 @@ function sum (int $a=0, int $b =0):int
     return $a +$b;
 }
 
-
-
 function diff( int $a=0, int $b=0): int
 {
     return $a - $b;
-}
-
-function div( int $a=0, int $b=0): int
-{
-    return $a / $b;
 }
 
 function mult( int $a=0, int $b=0): int
@@ -32,11 +25,17 @@ function mult( int $a=0, int $b=0): int
     return $a * $b;
 }
 
+function div( int $a=0, int $b=1): float
+{ if($b==0) {
+
+    die("");
+}
+    return $a / $b;
+}
 
 
 
-
-echo sum(5,3);
-echo diff(5,3);
-echo mult(4,9);
-echo div(5,3);
+echo sum(5,3),PHP_EOL;
+echo diff(5,3),PHP_EOL;
+echo mult(4,9),PHP_EOL;
+echo div(5,0),PHP_EOL;
