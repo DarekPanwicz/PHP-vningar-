3fae42c3cf7f6344281a4a154b0d94ea7107eeb1
Darek P <dajmos007@gmail.com>
2017-12-17 16:00:31 +0100

    new line

--- Customer.php
+++ Customer.php
@@ -6,17 +6,36 @@
  * Time: 09:28
  */
 
-declare(stric_type=1);
+//declare(strict_type=1);
 
 class Customer
 {
-    public $name;
-    $public function getDiscount(): int
+    // privatr, public, protected
+
+    private $name;
+    public function getDiscount():int
 
 {
-
     return 20;
 }
+
+// setter setName
+// getter getName
+
+
+    public function  setName(string $name): void
+{
+     $this->name;
+}
+
+
+    public function getName():string
+    {
+        return $this->name;
+    }
+
+
+
 
 
 }
--- index.php
+++ index.php
@@ -5,11 +5,12 @@
  * Date: 2017-12-17
  * Time: 15:15
  */
-declare(stric_type=1);
+//declare(strict_type=1);
 
 require_once 'Customer.php';
 
 $customer =new Customer();
-$customer->name = 'Marian';
+$customer->setName('Marian');
+echo $customer->getDiscount();
 
 var_export($customer);
