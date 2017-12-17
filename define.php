14860a3d7cb7db929b0ccd1615f181e1aa23ac28
Darek P <dajmos007@gmail.com>
2017-12-17 10:19:24 +0100

    New file

--- define.php
+++ define.php
@@ -5,6 +5,8 @@
  * Date: 2017-12-17
  * Time: 10:07
  */
+
+
 //Wlaczenie wyswietlania bledow
 
 error_reporting(E_ALL);
@@ -12,5 +14,10 @@ ini_set('display_errors', true);
 
 
 const MAX_SLIDERS_NUMBER = 99;
+if(!defined(MAX_SLIDERS_NUMBER){
+
+    defined("MAX_SLIDES_NUMBER", 666);
+}
+
 define("MAX_SLIDES", 666);
 echo "Maxymalna ilosc slajdow: ",MAX_SLIDERS_NUMBER, PHP_EOL;
