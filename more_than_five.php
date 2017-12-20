f85f19cb8ce624bc8ce15e2c5e4314bd4471b89f
dajmos <dajmos007@gmail.com>
2017-12-20 10:49:11 +0100

    Adding new if statement

--- more_than_five.php
+++ more_than_five.php
@@ -6,3 +6,22 @@
  * Time: 10:30
  */
 
+//New loop
+
+for($i=1; $i<=10; $i++){
+    //New if with continue statement
+    if($i<5){
+        continue;
+    }
+
+            if($i%2 ){
+
+                echo "liczba nieparzysta to: " . "<b>".$i .  "</b><br>";
+
+            } else {
+
+                echo "liczba parzysta to: " . "<b>".$i .  "</b><br>";
+
+            }
+
+}
