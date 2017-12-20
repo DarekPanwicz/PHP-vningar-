3228ee698e74dd3ea2c64724f85685ace5b273ed
dajmos <dajmos007@gmail.com>
2017-12-20 11:11:37 +0100

    Added new file and array

---
+++ loops.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-20
+ * Time: 10:53
+ */
+
+
+$months = ['styczen', 'luty', 'marzec', 'kwiecien', 'maj', 'czerwiec', 'lipiec', 'sierpien', 'wrzesien', 'pazdziernik', 'listopad', 'grudzien'];
