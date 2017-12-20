af540e8f658be10c64303f6f4a42f377aee623f7
dajmos <dajmos007@gmail.com>
2017-12-20 10:01:07 +0100

    Added new for loop

--- even.php
+++ even.php
@@ -6,3 +6,17 @@
  * Time: 09:44
  */
 
+
+//Adding new loop
+for($i=1; $i<=10; $i++){
+
+    if($i%2 ){
+
+
+    } else {
+
+        echo "liczba parzysta to: " . "<b>".$i .  "</b><br>";
+
+    }
+
+}
