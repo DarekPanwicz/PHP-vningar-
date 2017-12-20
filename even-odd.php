abb01895e21544692dd2bf6dfc1f2f97efdf3dc4
dajmos <dajmos007@gmail.com>
2017-12-20 10:29:08 +0100

    Adding new loop, even and odd

--- even-odd.php
+++ even-odd.php
@@ -6,8 +6,20 @@
  * Time: 10:19
  */
 
+//New loop
+for($i=1; $i<=10; $i++){
 
+    if($i%2 ){
 
+        echo "liczba nieparzysta to: " . "<b>".$i .  "</b><br>";
+
+    } else {
+
+        echo "liczba parzysta to: " . "<b>".$i .  "</b><br>";
+
+    }
+
+}
 
 
 
