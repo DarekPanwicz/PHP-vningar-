fdab7ab8cf69e4dd0d096e302f8103605ce1d673
dajmos <dajmos007@gmail.com>
2017-12-20 11:21:59 +0100

    Adding for and foreach loops

--- loops.php
+++ loops.php
@@ -8,3 +8,28 @@
 
 
 $months = ['styczen', 'luty', 'marzec', 'kwiecien', 'maj', 'czerwiec', 'lipiec', 'sierpien', 'wrzesien', 'pazdziernik', 'listopad', 'grudzien'];
+
+//przy pomocy poniższych petli po kolei:
+//- foreach
+//    - for
+//    - while
+
+
+//Foreach loop
+
+foreach ($months as $arr1) {
+
+    echo "Zawartosc Tab: " . $arr1 . "<br>";
+
+}
+echo "<br><hr><br>";
+
+// for loop
+for($i=0; $i<sizeof($months); $i++){
+
+    echo "Zawartosc Tab: " . $months[$i] . "<br>";
+
+}
+
+
+// while loop
