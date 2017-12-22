f0c46fd5757f6cba83d5805920ae1b98057e5abe
dajmos <dajmos007@gmail.com>
2017-12-22 12:22:11 +0100

    Adding new file exc1_array.php

---
+++ exc1_Array.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-22
+ * Time: 12:19
+ */
+
+$color = array('white', 'green', 'red', 'blue', 'black');
+// Write a script which will display the following string - Go to the editor
+//and the words 'red', 'green' and 'white' will come from $color.
+"The memory of that scene for me is like a frame of film forever frozen at that moment: the red carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon"
