ee80f712b741443093584ca96cee83e73ad0b2b5
dajmos <dajmos007@gmail.com>
2017-12-22 12:27:12 +0100

    Adding new variable from array

--- exc1_Array.php
+++ exc1_Array.php
@@ -9,4 +9,4 @@
 $color = array('white', 'green', 'red', 'blue', 'black');
 // Write a script which will display the following string - Go to the editor
 //and the words 'red', 'green' and 'white' will come from $color.
-"The memory of that scene for me is like a frame of film forever frozen at that moment: the red carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon"
+echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the <b> $color[2] </b> carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon";
