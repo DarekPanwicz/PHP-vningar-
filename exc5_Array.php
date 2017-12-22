38d6c471e5b5371f453e3c7b4f847ed81b2f3f28
dajmos <dajmos007@gmail.com>
2017-12-22 13:16:43 +0100

    Adding new functions 'json_decode and array_walk_recursive' git push

--- exc5_Array.php
+++ exc5_Array.php
@@ -10,13 +10,36 @@
 
 Sample JSON code :*/
 
-{"Title": "The Cuckoos Calling",
+/*{"Title": "The Cuckoos Calling",
 "Author": "Robert Galbraith",
 "Detail": {
-    "Publisher": "Little Brown"
-}}
+        "Publisher": "Little Brown"
+}};*/
+
 /*Expected Output :
 Title : The Cuckoos Calling
 Author : Robert Galbraith
 Publisher : Little Brown*/
+
+
+
+function w3rfunction($value,$key)
+{
+    echo "$key : $value"."\n";
+}
+
+
+
+$a = '{"Title": "The Cuckoos Calling",
+"Author": "Robert Galbraith",
+"Detail":
+{ 
+"Publisher": "Little Brown"
+ }
+  }';
+
+
+$j1 = json_decode($a,true);
+array_walk_recursive($j1,"w3rfunction");
+
 
