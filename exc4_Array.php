bb9765f6b1c22e62acae10b09cfe439868c74309
dajmos <dajmos007@gmail.com>
2017-12-22 12:53:37 +0100

    Adding new file exc4_Array

---
+++ exc4_Array.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-22
+ * Time: 12:51
+ */
+
+$color = array(4 => 'white', 6 => 'green', 11=> 'red');
+//Write a PHP script to get the first element of the above array.
+//Expected result : white
