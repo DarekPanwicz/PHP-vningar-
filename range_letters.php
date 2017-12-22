0fb6ae20b1c0f645345b23af253b8466ee065a05
dajmos <dajmos007@gmail.com>
2017-12-22 09:44:40 +0100

    Adding new function 'range'

---
+++ range_letters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-22
+ * Time: 09:41
+ */
+
+
+//Adding new loop with function range
+
+foreach (range(a,z, 1) as $currentNumber){
+
+    echo "aktualna liczba: " . $currentNumber . "<br>";
+
+}
