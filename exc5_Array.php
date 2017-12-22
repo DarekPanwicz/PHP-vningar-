3f594be227d36732404696584d9abc91a2a39cbd
dajmos <dajmos007@gmail.com>
2017-12-22 13:03:03 +0100

    Adding new file exc5_array.php, string/int to json

---
+++ exc5_Array.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-22
+ * Time: 13:00
+ */
+
+/*Write a PHP script which decodes the following JSON string.
+
+Sample JSON code :*/
+
+{"Title": "The Cuckoos Calling",
+"Author": "Robert Galbraith",
+"Detail": {
+    "Publisher": "Little Brown"
+}}
+/*Expected Output :
+Title : The Cuckoos Calling
+Author : Robert Galbraith
+Publisher : Little Brown*/
+
