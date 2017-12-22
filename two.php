a3cc0bd28f31625ff2aadd8bb2eda5ef6a141a72
dajmos <dajmos007@gmail.com>
2017-12-22 09:04:59 +0100

    Adding new file

---
+++ two.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-22
+ * Time: 09:02
+ */
+/*
+Korzystając z pętli for napisz tabliczkę mnożenia, która wyświetli wynik:
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+2 * 6 = 12
+2 * 7 = 14
+2 * 8 = 16
+2 * 9 = 18*/
+
+
+for ($i=0; $i<=8; $i ++){
+
+
+
+}
