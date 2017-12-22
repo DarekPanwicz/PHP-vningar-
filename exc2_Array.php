22f35c7f9ad3ab99fa9d74079fe7f54d31f00718
dajmos <dajmos007@gmail.com>
2017-12-22 12:33:11 +0100

    Adding new file

---
+++ exc2_Array.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-22
+ * Time: 12:32
+ */
+
+$color = array('white', 'green', 'red'') Go to the editor
+Write a PHP script which will display the colors in the following way : 
+Output : 
+white, green, red,
+
+green
+red
+white
