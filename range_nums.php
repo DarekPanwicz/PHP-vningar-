f16892267fdabe13bd80f553004c2a2158e10c75
dajmos <dajmos007@gmail.com>
2017-12-22 09:33:23 +0100

    Adding new array

--- range_nums.php
+++ range_nums.php
@@ -6,3 +6,12 @@
  * Time: 09:26
  */
 
+/*ZAD. 13.
+Stwórz tablicę przy pomocy funkcji range(0, 50):
+http://php.net/manual/pl/function.range.php
+Przy pomocy pętli foreach przeiteruj się przez każdy element i wyświetl na output.
+Skrypt zapisz jako range_nums.php.*/
+
+$arr1[];
+
+$arr1 range
