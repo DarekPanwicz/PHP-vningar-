ac6828bf00fe9f908b1c1f32cc7a9a772ec28184
dajmos <dajmos007@gmail.com>
2017-12-22 09:13:48 +0100

    New file two_fors.php

---
+++ two_fors.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dajmos
+ * Date: 2017-12-22
+ * Time: 09:11
+ */
+
+/*AD. 12.
+Korzystając z dwóch zagnieżdżonych pętli for napisz tabliczkę mnożenia, która wyświetli mnożenie razy dwa, razy trzy, razy cztery i razy pięć.
+Plik zapisz jako two_fors.php.*/
+
