d771c43f08ce08bcf53c2b6302fd395e6847fbb4
dajmos <dajmos007@gmail.com>
2017-12-23 09:27:40 +0100

    Adding new file exc6_array

---
+++ exc6_Array.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2017-12-23
+ * Time: 09:26
+ */
+
+/*7. Write a PHP script that inserts a new item in an array in any position.
+Expected Output :
+Original array :
+1 2 3 4 5
+After inserting '$' the array is :
+1 2 3 $ 4 5*/
+
