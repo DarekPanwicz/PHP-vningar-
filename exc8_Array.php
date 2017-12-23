5cfe847be9b3dafac15759f91df23770d8664dfb
dajmos <dajmos007@gmail.com>
2017-12-23 11:23:31 +0100

    Adding new asort function and for loop

--- exc8_Array.php
+++ exc8_Array.php
@@ -6,9 +6,44 @@
  * Time: 10:39
  */
 
-/*Write a PHP script to calculate and display average temperature, five lowest and highest temperatures. Go to the editor
+/*Write a PHP script to calculate and display average temperature, five lowest and highest temperatures.
 Recorded temperatures : 78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73
 Expected Output :
 Average Temperature is : 70.6
 List of seven lowest temperatures : 60, 62, 63, 63, 64,
 List of seven highest temperatures : 76, 78, 79, 81, 85,*/
+
+
+$monthTemp = "8, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73";
+
+$tempArray= explode(',',$monthTemp);
+$totalTemp= 0;
+$tempArrayLength =count($tempArray);
+
+foreach ($tempArray as $temp){
+
+    $totalTemp+=$temp;
+
+}
+
+$avgHightTemp = $totalTemp/$tempArrayLength;
+echo "Avrage temp is: " . $avgHightTemp;
+
+echo "<br><hr>";
+
+
+asort($tempArray);
+
+
+for($i=0; $i<7; $i++){
+
+    echo $tempArray[$i];
+
+}
+
+
+
+
+
+
+
