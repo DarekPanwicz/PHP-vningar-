70ec13998b1bedc7a4e71f85941eaba4eb6f240d
dajmos <dajmos007@gmail.com>
2017-12-27 10:37:44 +0100

    Adding new for loop

--- exc9_Array.php
+++ exc9_Array.php
@@ -8,3 +8,15 @@
 
 
 //Create a script using a for loop to add all the integers between 0 and 30 and display the total.
+
+$sum= 0;
+
+// Adding new loop
+for ( $i=1; $i<=30; $i++){
+
+    $sum +=$i;
+}
+
+echo "the total sum of the 0 to 30 is " . $sum;
+
+
