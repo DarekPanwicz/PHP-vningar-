77a0b33606780f842b439b6003ca0562ffda014c
dajmos <dajmos007@gmail.com>
2017-12-27 11:05:33 +0100

    Adding two loops

--- exc2_loop.php
+++ exc2_loop.php
@@ -5,3 +5,28 @@
  * Date: 2017-12-27
  * Time: 10:49
  */
+//
+//Create a script to construct the following pattern, using nested for loop.
+//
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+
+
+
+
+for ($i=0; $i<5; $i++){
+
+  for ($j=0; $j<=$i; $j++){
+
+      echo  "*";
+   /*   if($i<$j){
+
+          echo " ";
+      }*/
+  }
+ echo " <br>";
+}
+
