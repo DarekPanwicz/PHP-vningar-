96a95b80b87660ad310012f91eb02cefa2c3f16a
dajmos <dajmos007@gmail.com>
2017-12-27 10:51:33 +0100

    changing file name

--- exc1_loop.php
+++ exc1_loop.php
@@ -9,10 +9,11 @@
 
 //Create a script using a for loop to add all the integers between 0 and 30 and display the total.
 
-$sum= 0;
+$sum= 1;
 
 // Adding new loop
 for ( $i=1; $i<=30; $i++){
+    echo $sum . " <br>";
 
     $sum +=$i;
 }
---
+++ exc2_loop.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2017-12-27
+ * Time: 10:49
+ */
