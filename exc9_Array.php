0ec56dd86a7073b95bc0ccc0ef28d4ecc1819d61
dajmos <dajmos007@gmail.com>
2017-12-27 10:30:55 +0100

    New file exc9

---
+++ exc9_Array.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2017-12-27
+ * Time: 10:23
+ */
+
+
+//Create a script using a for loop to add all the integers between 0 and 30 and display the total.
