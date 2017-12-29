60ce3983cf5db89fc7f2150d0b856d228402aca1
dajmos <dajmos007@gmail.com>
2017-12-29 10:55:58 +0100

    Added new file

---
+++ exc3_loop.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2017-12-29
+ * Time: 10:55
+ */
+
+4. Create a script to construct the following pattern, using a nested for loop.
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
