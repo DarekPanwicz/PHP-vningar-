12cbee0be33136532d8f51912e2d81f4837fb771
Darek P <dajmos007@gmail.com>
2018-01-20 15:47:56 +0100

    Adding new classes

---
+++ ContactLens.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2018-01-20
+ * Time: 15:45
+ */
+
+class ContactLens extends Glasses
+{
+
+
+
+
+}
---
+++ Glasses.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2018-01-20
+ * Time: 15:46
+ */
+
+class Glasses
+{
+    //This function have only Glasses
+     public function chooseFrame();
+}
