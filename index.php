677590ea0695e23606c61726eaaac2213e422c01
Darek P <dajmos007@gmail.com>
2018-01-27 15:43:49 +0100

    Adding new index.php file

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2018-01-27
+ * Time: 15:43
+ */
--- main.php
+++ main.php
@@ -7,10 +7,9 @@
  */
 declare(strict_types=1);
 
+//Trzeba doinstalowac sobie yamla
 $config = yaml_parse_file('config.yml');
 var_export($config);
-
-
 
 //Getting file from webb
 $getPoster = file_get_contents('https://upload.wikimedia.org/wikipedia/en/4/4f/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg');
@@ -52,19 +51,17 @@ foreach ($matches[1] as $filename) {
 
     $time = new DateTime();
     file_put_contents("logg/filmoteka.log", "Nr: $filename" ." ,". $time->format('Y-m-d H:i:s') . " Zakonczenie zapisywania plakatu:" . $titles[$filename-1] . PHP_EOL, FILE_APPEND);
-
-
 }
 
 
 //Adding new loop for saving 12 shots files from webb (getShots)
-//for ($matches[1] as $filename) {
-//
-//    $time = new DateTime();
-//    file_put_contents("logg/filmoteka.log", "Nr: $filename" ." ,".$time->format('Y-m-d H:i:s') . " Rozpoczecie pobierania plakatu:" . $titles[$filename-1] . PHP_EOL, FILE_APPEND);
-//    //Getting file list from webb - shots
-//    $getShots = file_get_contents('https://cytaty.eu/img/sda/shots/' . $filename . '.jpg');
-//
-//    file_put_contents("shots/" . $i . ".jpg", $getShots);
-//}
+foreach ($matches[1] as $filename) {
+
+   $time = new DateTime();
+    file_put_contents("logg/filmoteka.log", "Nr: $filename" ." ,".$time->format('Y-m-d H:i:s') . " Rozpoczecie pobierania plakatu:" . $titles[$filename-1] . PHP_EOL, FILE_APPEND);
+    //Getting file list from webb - shots
+   $getShots = file_get_contents('https://cytaty.eu/img/sda/shots/' . $filename . '.jpg');
+
+   file_put_contents("shots/" . $i . ".jpg", $getShots);
+}
 
