1b6c82faa8115465e0b39b6c0f77a141e3db247c
Darek P <dajmos007@gmail.com>
2018-01-27 16:02:50 +0100

    Adding new classes picture, logger, helper, filesdownloader, config

---
+++ src/Dajmos007/FilesDownloader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2018-01-27
+ * Time: 15:59
+ */
+declare(strict_types=1);
+
+namespace Dajmos007;
+
+class FilesDownloader
+{
+
+}
--- src/Dajmos007/PosterD.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Darek P
- * Date: 2018-01-27
- * Time: 09:39
- */
-
-
