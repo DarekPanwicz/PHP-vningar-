206367e6d08524fd376e3a2a59e25c504d22ece8
Darek P <dajmos007@gmail.com>
2018-01-27 10:44:05 +0100

    Adding new for loop

--- main.php
+++ main.php
@@ -13,3 +13,15 @@ $getPoster = file_get_contents('https://upload.wikimedia.org/wikipedia/en/4/4f/T
 //Converting string to image and saving picture in poster folder
 file_put_contents('posters/img.jpg', $getPoster);
 
+//Getting file list from webb
+$getPosters = file_get_contents('https://cytaty.eu/img/sda/posters/');
+
+
+//Adding new loop for saving 12 files from webb (getposters)
+for($i=1; $i<=12;$i++)
+{
+    //Getting file list from webb
+    $getPosters = file_get_contents('https://cytaty.eu/img/sda/posters/'.$i.'.jpg');
+
+    file_put_contents("posters/" .$i . ".jpg", $getPosters);
+}
