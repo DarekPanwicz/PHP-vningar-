538b83c4a477408836a706d0eea9b130a3ce7640
Darek P <dajmos007@gmail.com>
2018-01-28 10:35:10 +0100

    Adding new config file

---
+++ src/Dajmos007/Config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darek P
+ * Date: 2018-01-27
+ * Time: 16:00
+ */
+declare(strict_types=1);
+
+namespace Dajmos007;
+
+class Config
+{
+
+    public const GET_POSTERS_LINKS= "https://cytaty.eu/img/sda/posters/";
+    public const GET_SHOTS_LINKS= "https://cytaty.eu/img/sda/shots/";
+    public const SAVE_POSTERS_LINKS="/img/posters";
+    public const SAVE_SHOTS_LINKS="/img/shots";
+    public const TITLES=
+
+        [
+            "Pirates of Carribean",
+            "Dead Men tell No Tales",
+            "Rings",
+            "Blade Runner 2049",
+            "Thor",
+            "Get Out",
+            "Star Wars: The Last Jedi",
+            "Okja", "London Has Fallen",
+            "The Legend of Tarzan",
+            'The Founder',
+            "Captain America: Civil War",
+            "Spectre"
+        ];
+
+
+    public const LOGS = "/logs";
+
+
+}
