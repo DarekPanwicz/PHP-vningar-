5b5fada907323ac3577e886d7ef65e2fad613ac7
Darek P <dajmos007@gmail.com>
2018-01-28 12:16:42 +0100

    Removed spacing and adding new comments to main.php and loger.php

--- index.php
+++ index.php
@@ -10,47 +10,52 @@ declare(strict_types=1);
 
 namespace Dajmos007;
 
+//Adding autoload. package downloaded with composer (vendor->composer).
 require __DIR__ . '/vendor/autoload.php';
 
-//Copying all posters pictures from webbsite
+//Copying all posters pictures from webbsite.
 $getPosters = file_get_contents(Config::GET_POSTERS_LINKS);
 
 $matches= [];
 
-/// REGEXP - taking pattern from links on the website to calculate number of files to import
+/// REGEXP - taking pattern from links on the website to calculate number of files to import.
 preg_match_all( '/href="([0-9]+)\.jpg"/', $getPosters, $matches);
 
-// Initiating new object, part of class Loger
+// Initiating new object, part of class Loger.
 $logger= new Loger();
-//Lopp with posters
+//Lopp with posters.
 foreach ($matches[1] as $filename)
 {
 
-    //Save START time, string in the system log
+    //Save START time, string in the system log.
     $logger->addTologer( " Rozpoczecie pobierania PLAKATU:" . Config::TITLES[$filename-1]);
 
-    //Getting posters file list from webb
+    //Getting posters file list from webb.
     $getPosters = file_get_contents(Config::GET_POSTERS_LINKS . $filename . '.jpg');
-    //Save all Posters Files to img posters
-    file_put_contents(Config::SAVE_POSTERS_LINKS . str_ireplace([' ', ':'],['-',''],Config::TITLES[$filename-1]) . ".jpg", $getPosters);
+    //Save all Posters Files to img posters.
+    file_put_contents(Config::SAVE_POSTERS_LINKS
+        . str_ireplace([' ', ':'],['-',''], Config::TITLES[$filename-1])
+        . ".jpg", $getPosters);
 
-    //Save END time, string in the system log
+    //Save END time, string in the system log.
     $logger->addTologer( " Zakonczenie pobierania PLAKATU:" . Config::TITLES[$filename-1]);
 }
 
-//Lopp with shots
+//Lopp with shots.
 foreach ($matches[1] as $filename)
 {
 
-    //Save START time, string in the system log
+    //Save START time, string in the system log.
     $logger->addTologer( " Rozpoczecie pobierania SHOTA:" . Config::TITLES[$filename-1]);
 
-    //Getting shots file list from webb
+    //Getting shots file list from webb.
     $getPosters = file_get_contents(Config::GET_SHOTS_LINKS . $filename . '.jpg');
-    //Save all Posters Files to img posters
-    file_put_contents(Config::SAVE_SHOTS_LINKS . str_ireplace([' ', ':'],['-',''],Config::TITLES[$filename-1]) . ".jpg", $getPosters);
+    //Save all Posters Files to img posters.
+    file_put_contents(Config::SAVE_SHOTS_LINKS
+        . str_ireplace([' ', ':'],['-',''],Config::TITLES[$filename-1])
+        . ".jpg", $getPosters);
 
-    //Save END time, string in the system log
+    //Save END time, string in the system log.
     $logger->addTologer( " Zakonczenie pobierania SHOTA:" . Config::TITLES[$filename-1]);
 }
 
--- src/Dajmos007/Loger.php
+++ src/Dajmos007/Loger.php
@@ -9,16 +9,18 @@ declare(strict_types=1);
 
 namespace Dajmos007;
 
+//This class is responsible for log system which is define in index.php
 class Loger
 {
 
-    //Function that adds logs to system
-    public function addTologer($loggerText)
+    //Function that adds logs to system, with string $logerText variable
+    public function addTologer(string $loggerText)
 
     {
         $time = new \DateTime();
         //Save time, string with object name, in the system log
-        file_put_contents(Config::LOGS, $time->format('Y-m-d H:i:s') . $loggerText . PHP_EOL, FILE_APPEND);
+        file_put_contents(Config::LOGS, $time->format('Y-m-d H:i:s')
+            . $loggerText . PHP_EOL, FILE_APPEND);
 
     }
 
