a5d2ac1abf2effdacf67972c938a4e82838d3e7c
dajmos <dajmos007@gmail.com>
2018-02-23 14:00:50 +0100

    Adding comments

--- main.php
+++ main.php
@@ -1,24 +1,17 @@
 <?php
-//
-//ZAD. 1. KOSTKA / WARIANT PROSTY
-//Napisz prostą aplikację, która zapyta o imię użytkownika, a następnie wyświetli komunikat:
-//[WCIŚNIJ ENTER, ABY RZUCIĆ KOSTKĄ]
-//i gdy użytkownik wciśnie ENTER, wyświetli się wylosowana wartość, a po chwili wartość wylosowana dla gracza KOMPUTER.
-//Jeśli wartość gracza KOMPUTER jest większa, wygrywa KOMPUTER.
-//Jeśli wartość gracza, którym jest użytkownik, jest większa, wygrywa użytkownik.
-//W przypadku jednakowych wartości, jest remis.
-
-
-
 
 
 echo "Jak masz na imie: ";
+//Funkcja ktora pyta o imput
 $imie= fgets(STDIN);
 echo "[WCIŚNIJ ENTER, ABY RZUCIĆ KOSTKĄ]";
 fgets(STDIN);
 echo " " .PHP_EOL;
+
+//Dodanie systemu losowania
 $losowanieGracz= rand ( 1, 6);
 $losowanieAi= rand ( 1, 6);
+
 echo "Czesc " . $imie . " Wylosowales liczbe:" . " " . $losowanieGracz . PHP_EOL." A najnowszej generacji AI wylosowalo liczbe: " . $losowanieAi .PHP_EOL;
 
 if ($losowanieGracz < $losowanieAi)
